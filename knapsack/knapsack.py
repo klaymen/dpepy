5c5fdb3d6788d9d6f5b13884e0bcbeb7b0915d1e
klaymen <klaymen@spacebar.hu>
2019-07-30 09:45:11 +0200

    add knapsack problem description

---
+++ knapsack/knapsack.py
@@ -0,0 +1,5 @@
+ # Description: 
+ # Given a set of items, each with a weight and a value, determine the number
+ # of each item to include in a collection so that the total weight is less
+ # than or equal to a given limit and the total value is as large as possible.
+ # [wiki] https://en.wikipedia.org/wiki/Knapsack_problem
