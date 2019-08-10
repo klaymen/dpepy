8ed609c85fd4a76d593b2bc67cffb1d95d70ded0
klaymen <klaymen@spacebar.hu>
2019-08-10 14:31:31 +0200

    add comments

--- longest_increasing_subsequence/longest_increasing_subsequence.py
+++ longest_increasing_subsequence/longest_increasing_subsequence.py
@@ -3,6 +3,8 @@
  # Complexity: O(n^2)
 
 def longest_increasing_subsequence(arr):
+    # Initialize the result with ones, since they are the last element of an at 
+    # least 1 long subsequence.
     res = [1] * len(arr)
 
     # Go over the items from the second and check if they satisfy our conditions
@@ -12,7 +14,10 @@ def longest_increasing_subsequence(arr):
             # If yes AND they yield a longer sequence, then update the results
             if arr[i] > arr [j] and res[j] + 1 > res[i]:
                 res[i] = res[j] + 1
-            
+    
+    # As the result array is made of the length of the 
+    # Longest increasing subsequence ending with the given index
+    # We only need tho return the maximum of it.
     return max(res)
 
 print(longest_increasing_subsequence([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]))
